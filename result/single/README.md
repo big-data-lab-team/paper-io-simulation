59270bf9993a642524cfe3191fddb2b47ff0406f
Dung Do <dzung.do@fruitful.io>
2020-10-19 12:24:54 -0400

    Update cache plot and add notebook

--- result/single/README.md
+++ result/single/README.md
@@ -1,5 +1,10 @@
 This is the directory to place and analyze the results of the single-threaded experiment.
 
+### Before analyzing the results
+Run the pipeline on a real system or run simulators, copy the log files and put in 
+corresponding directories.
+
+#### Memory profiling
 - The output log files from the real pipeline should be placed in `real/<size_in_gb>gb/` directory.
 There are 3 required log files: `atop` logfile (named as `atop_mem.log`), `collectl` logfile 
 (named as `collectl.dsk`), and `timestamps.csv`.
@@ -16,15 +21,17 @@ There is only one log file for each input file size named `<file_size_gb>gb_sim_
 There are 2 logfiles for each input file size: `<file_size_gb>gb_sim_mem.csv` 
 and `<file_size_gb>gb_sim_time.csv`
 
-- There are 2 logfiles of each simulator for each input file size: `<file_size_gb>gb_sim_mem.csv` 
+- There are 2 log files of each simulator for each input file size: `<file_size_gb>gb_sim_mem.csv` 
 and `<file_size_gb>gb_sim_time.csv`
 
-#### Before analyzing the results
-Run the pipeline on a real system or run simulators, copy the log files and put in 
-corresponding directories.
+#### Cached files
+- Cached amount of files are recorded and stored in `fincore` directory, 
+`real.csv` from the real cluster and `sim.csv` from WRENCH simulator with page cache.
 
-#### Results from the real pipeline
-Open the jupyter notebook `plot_real_result.ipynb`
+- Instructions are in the jupyter notebook `plot_cache.ipynb` 
 
-#### Errors comparison between python simulator and WRENCH simulator
-Open the jupyter notebook `plot_error.ipynb` 
+### Results from the real pipeline
+Instructions are in the jupyter notebook `plot_real_result.ipynb`
+
+### Errors comparison between python simulator and WRENCH simulator
+Instructions are in the jupyter notebook `plot_error.ipynb` 
--- result/single/plot_fincore.py
+++ result/single/plot_fincore.py
@@ -1,38 +1,48 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-columns = ["read1", "write1", "read2", "write2", "read3", "write3"]
-files = ["file1", "file2", "file3", "file4"]
-df_real = pd.read_csv("fincore/real.csv", index_col="task")
-df_sim = pd.read_csv("fincore/sim.csv", index_col="task")
-patterns = ["", "...", "///", "\\\\\\",]
 
-# axe = df_real[files].plot.bar(stacked=True, rot=0, color="tab:purple", edgecolor="k")
-axe = df_sim[files].plot.bar(stacked=True, rot=0, color="tab:purple", edgecolor="k")
+def plot_cache(file, source):
+    columns = ["read1", "write1", "read2", "write2", "read3", "write3"]
+    files = ["file1", "file2", "file3", "file4"]
 
-bars = axe.patches
-hatches = []  # list for hatches in the order of the bars
-for h in patterns:  # loop over patterns to create bar-ordered hatches
-    for i in range(int(len(bars) / len(patterns))):
-        hatches.append(h)
-for bar, hatch in zip(bars, hatches):  # loop over bars and hatches to set hatches in correct order
-    bar.set_hatch(hatch)
+    if source == "real":
+        df = pd.read_csv("fincore/real.csv", index_col="task")
+    if source == "wrench":
+        df = pd.read_csv("fincore/sim.csv", index_col="task")
 
-plt.subplots_adjust(left=0.12, bottom=0.15, right=0.92, top=0.9)
+    patterns = ["", "...", "///", "\\\\\\"]
 
-plt.xlabel("")
-plt.ylabel("amount (GB)")
+    # axe = df_real[files].plot.bar(stacked=True, rot=0, color="tab:purple", edgecolor="k")
+    axe = df[files].plot.bar(stacked=True, rot=0, color="tab:purple", edgecolor="k")
 
-legend = plt.legend()
-l_hatches = []
-legends = legend.axes.patches
-for h in patterns:  # loop over patterns to create bar-ordered hatches
-    for i in range(int(len(legends) / len(patterns))):
-        l_hatches.append(h)
-for bar, hatch in zip(legends, l_hatches):  # loop over bars and hatches to set hatches in correct order
-    bar.set_hatch(hatch)
+    bars = axe.patches
+    hatches = []  # list for hatches in the order of the bars
+    for h in patterns:  # loop over patterns to create bar-ordered hatches
+        for i in range(int(len(bars) / len(patterns))):
+            hatches.append(h)
+    for bar, hatch in zip(bars, hatches):  # loop over bars and hatches to set hatches in correct order
+        bar.set_hatch(hatch)
 
-plt.savefig("figures/fincore_wrench.svg", format="svg")
-plt.savefig("figures/fincore_wrench.pdf", format="pdf")
+    plt.subplots_adjust(left=0.12, bottom=0.15, right=0.92, top=0.9)
 
-plt.show()
+    plt.xlabel("")
+    plt.ylabel("amount (GB)")
+
+    legend = plt.legend()
+    l_hatches = []
+    legends = legend.axes.patches
+    for h in patterns:  # loop over patterns to create bar-ordered hatches
+        for i in range(int(len(legends) / len(patterns))):
+            l_hatches.append(h)
+    for bar, hatch in zip(legends, l_hatches):  # loop over bars and hatches to set hatches in correct order
+        bar.set_hatch(hatch)
+
+    plt.savefig("figures/fincore_%s.svg" % source, format="svg")
+    plt.savefig("figures/fincore_%s.pdf" % source, format="pdf")
+
+    plt.show()
+
+
+plot_cache("fincore/real.csv", "real")
+plot_cache("fincore/wrench.csv", "wrench")
