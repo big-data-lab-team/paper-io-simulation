6fdc00abf04c74c38e3b5a070d4e22997b4af8b8
Tristan Glatard <tristan.glatard@concordia.ca>
2020-12-08 19:53:30 -0500

    fixed figure 4

--- result/single/plot_cache.py
+++ result/single/plot_cache.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 
 
-def plot_cache(ax, real_file, sim_file, patterns, color_real="gainsboro", color_wrench="tab:cyan"):
+def plot_cache(ax, real_file, sim_file, patterns, ylabel, color_real="#90C987", color_wrench="#1965B0"):
     index = ["file_1", "file_2", "file_3", "file_4"]
     columns = ["Read 1", "Write 1", "Read 2", "Write 2", "Read 3", "Write 3"]
 
@@ -13,8 +13,8 @@ def plot_cache(ax, real_file, sim_file, patterns, color_real="gainsboro", color_
     df_real[index].plot.bar(stacked=True, rot=0, color=color_real, edgecolor="k", ax=ax, position=1, width=0.3, legend=False, linewidth=0.5)
     df_sim[index].plot.bar(stacked=True, rot=0, color=color_wrench, edgecolor="k", ax=ax, position=0, width=0.3, legend=False, linewidth=0.5)
 
-    ax.set_xlabel("tasks")
-    ax.set_ylabel("amount (GB)")
+    if ylabel:
+        ax.set_ylabel("amount (GB)")
     ax.set_xticklabels(columns)
 
     bars = ax.patches
@@ -28,6 +28,8 @@ def plot_cache(ax, real_file, sim_file, patterns, color_real="gainsboro", color_
     for bar, hatch in zip(bars, hatches):  # loop over bars and hatches to set hatches in correct order
         bar.set_hatch(hatch)
 
+    x_axis = ax.xaxis
+    x_axis.label.set_visible(False)
     ax.set_xlim(left=-0.5)
     ax.set_ylim(top=180)
 
@@ -37,15 +39,15 @@ def plot_cache_v2():
     files = ["file 1", "file 2", "file 3", "file 4"]
     envs = ["Real execution", "WRENCH-cache"]
     patterns = ["", "...", "///", "\\\\\\"]
-    color_real = "gainsboro"
-    color_wrench = "tab:cyan"
+    color_real = "#90C987"
+    color_wrench = "#1965B0"
 
-    fig, (ax1, ax2) = plt.subplots(figsize=(11, 4), ncols=2, nrows=1)
-    plt.subplots_adjust(left=0.1, bottom=0.15, right=0.95, top=0.8, wspace=0.25)
+    fig, (ax1, ax2) = plt.subplots(figsize=(11, 3), ncols=2, nrows=1)
+    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.95, top=0.8, wspace=0.1)
     plt.rcParams.update({'font.size': 9})
 
-    plot_cache(ax1, "fincore/real_20gb.csv", "fincore/sim_20gb.csv", patterns)
-    plot_cache(ax2, "fincore/real_100gb.csv", "fincore/sim_100gb.csv", patterns)
+    plot_cache(ax1, "fincore/real_20gb.csv", "fincore/sim_20gb.csv", patterns, ylabel=True)
+    plot_cache(ax2, "fincore/real_100gb.csv", "fincore/sim_100gb.csv", patterns, ylabel=False)
 
     legend_elements = [Patch(facecolor=color_real),
                        Patch(facecolor=color_wrench),
@@ -57,7 +59,7 @@ def plot_cache_v2():
     ax1.set_title("20 GB")
     ax2.set_title("100 GB")
 
-    plt.legend(legend_elements, envs + files, ncol=3, loc='upper center', bbox_to_anchor=(-0.1, 1.3))
+    plt.legend(legend_elements, envs + files, ncol=6, loc='upper center', bbox_to_anchor=(-0.1, 1.3))
     plt.savefig("figures/cached_files.svg", format="svg")
     plt.savefig("figures/cached_files.pdf", format="pdf")
 
