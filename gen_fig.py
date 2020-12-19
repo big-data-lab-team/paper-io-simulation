23f980a6da5d0f3cb252e9556e818a2a77b6f61f
Dung Do <dzung.do@fruitful.io>
2020-12-18 19:13:45 -0500

    Update exp2 results

--- gen_fig.py
+++ gen_fig.py
@@ -1,14 +1,16 @@
 import os
 import sys
-sys.path.append(os.path.abspath("result/single/"))
-sys.path.append(os.path.abspath("result/multi/"))
-
-
-# ======== Single-threaded experiment results ==========
-os.chdir("result/single")
 import result.single.plot_memprof as plot_memprof
 import result.single.plot_error as plot_error
 import result.single.plot_cache as plot_cache
+import result.multi.process_result as multi_result
+import result.nighres.process_result as nighres_result
+
+sys.path.append(os.path.abspath("result/single/"))
+sys.path.append(os.path.abspath("result/multi/"))
+
+# ======== Single-threaded experiment results ==========
+os.chdir("result/single")
 
 # Generate memory profiling comparision between 20GB and 100GB inputs.
 # xmax is set to 165 and 1300
@@ -20,9 +22,8 @@ plot_cache.plot_cache_v2()
 
 # ======== Multi-threaded experiment results ==========
 os.chdir("../multi")
-import result.multi.process_result as multi_result
 # Generate figures of experiment with local file system
-multi_result.result_local(rep_no=2, step=5)
+multi_result.result_local(rep_no=5, step=5)
 # Generate figures of experiment with NFS
 multi_result.result_nfs(rep_no=5)
 # runtime plots
@@ -30,5 +31,4 @@ multi_result.run_time()
 
 # ======== Nighres experiment results ==========
 os.chdir("../nighres")
-import result.nighres.process_result as nighres_result
 nighres_result.plot_error()
